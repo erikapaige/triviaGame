c38165eab31cff695841ffa47db421a38a144845
Erika <erika.paige.handley@gmail.com>
2020-05-13 11:10:33 -0700

    psuedocode

---
+++ psuedocode.js
@@ -0,0 +1,17 @@
+//1- install inquirer, chalk, axios
+//2- build index.html
+//3 - build index.js
+  // prompt user to select : leaderboard or new game
+
+  //LEADERBOARD
+    //1 
+
+  //NEW GAME
+    //1 - display lsit of available categories
+      //look at API
+      //look at inquirer list prompt
+      //build const var set equal to types of categories?
+    //2 - after selected category prompt with 10 questions
+      //const var with 10 questions form each category
+      // set var equal to prompt
+      
